de389af8ded4ef6fd9a90c2f94e545768fe84ce1
gus3000 <gus3000spam@gmail.com>
2024-03-01 23:05:40 +0100

    feat: rework Tetris sprites to a spritesheet

--- Tetris/gus3000/scripts/block.gd
+++ Tetris/gus3000/scripts/block.gd
@@ -3,13 +3,13 @@ extends Node2D
 enum BlockType {I, O, T, L, J, Z, S}
 @export var type: BlockType
 
-var I_BLOCK: Texture2D = preload ("res://art/I.png")
-var O_BLOCK: Texture2D = preload ("res://art/O.png")
-var T_BLOCK: Texture2D = preload ("res://art/T.png")
-var L_BLOCK: Texture2D = preload ("res://art/L.png")
-var J_BLOCK: Texture2D = preload ("res://art/J.png")
-var Z_BLOCK: Texture2D = preload ("res://art/Z.png")
-var S_BLOCK: Texture2D = preload ("res://art/S.png")
+var I_BLOCK: Texture2D = preload ("res://art/tetris_block_I.tres")
+var O_BLOCK: Texture2D = preload ("res://art/tetris_block_O.tres")
+var T_BLOCK: Texture2D = preload ("res://art/tetris_block_T.tres")
+var L_BLOCK: Texture2D = preload ("res://art/tetris_block_L.tres")
+var J_BLOCK: Texture2D = preload ("res://art/tetris_block_J.tres")
+var Z_BLOCK: Texture2D = preload ("res://art/tetris_block_Z.tres")
+var S_BLOCK: Texture2D = preload ("res://art/tetris_block_S.tres")
 
 var sprite: Sprite2D
 
